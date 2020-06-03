36227676496bdef2312a7208cc5c331eee049c64
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-06-03 12:18:54 +0200

    verwijderen van cypress

--- public/test/hello_world.spec.js
+++
@@ -1,7 +0,0 @@
-var expect = chai.expect;
-
-describe("De Hello World Test", function() {
-    it("de functie helloWorld zou 'HelloWorld' terug moeten geven", function() {
-        expect(helloWorld()).to.be.equal('Hello World');
-    });
-});
